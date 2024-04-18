f1e8dfc0b3d7bf78e1f96970b9b8e2c872b78727
Gonzalo Martinez <34680110ax@gamil.com>
2024-04-18 09:42:09 -0300

    new paths and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
-import { Home } from './pages/Home/Home'
 import { Route, Routes } from 'react-router-dom'
+// pages
+import { Home } from './pages/Home/Home'
 import { SearchList } from './pages/SearchList/SearchList'
 import { DetailBook } from './pages/Detail/DetailBook'
+import{AboutUsSection} from './pages/AboutUs/AboutUsSection'
+import{AllBooks} from './pages/AllBooks/AllBooks'
+import{Authors} from './pages/Authors/Authors'
+import{BuyPage} from './pages/BuyPage/BuyPage'
+import{Contact} from './pages/Contact/Contact'
+//componentes
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
 
-//  faltan agregar rutas : Contacto, Quienes somos, Carrito, allBooks, Autores, etc
 function App() {
 
 
@@ -17,6 +23,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/SearchList" element={<SearchList />} />
         <Route path="/DetailBook" element={<DetailBook />} />
+        <Route path="/AboutUs" element={<AboutUsSection />} />
+        <Route path="/AllBooks" element={<AllBooks />} />
+        <Route path="/Authors" element={<Authors />} />
+        <Route path="/BuyPage" element={<BuyPage />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
       <Footer /> 
     </div>
---
+++ src/pages/AboutUsSection/AboutUsSection.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AboutUsSection = () => {
+  return (
+    <div>AboutUsSection</div>
+  )
+}
