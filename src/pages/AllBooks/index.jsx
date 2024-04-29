28f86cf38c98e32024b7c08cde2fff265a98fe05
Juan Beresiarte <juanberesiarte@gmail.com>
2024-04-29 18:45:43 -0300

    Use mocked data

--- src/pages/AllBooks/index.jsx
+++ src/pages/AllBooks/index.jsx
@@ -1,21 +1,17 @@
-import styles from './AllBooks.module.scss'
-import { Card } from '../../components/Card/Card'
-import useBook from '../../hooks/useBook';
-import useFetch from '../../hooks/useFetch';
+import styles from './AllBooks.module.scss';
+import { Card } from '../../components/Card/Card';
+
+import books from "../../DMockeado/books";
 
 export default function AllBooks() {
-
-  const { getAllBooks } = useBook();
-  const books = useFetch(getAllBooks);
 
   return (
     <div className={styles.container} >
       <h1 className={styles.title}>All Books</h1>
       <div className={styles.booksContainer}>
-        {books.loading && <span>Cargando...</span>}
-        {!books.loading && books.result.map(book => (
-          <Card key={book.id} book={book}></Card>
-        ))}
+        {books?.map((item) => {
+          return <Card key={item.book.id + "AB"} book={item.book} />
+        })}
       </div>
     </div>
   )
